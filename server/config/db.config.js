704872760ff9e71089bb09544e71747e4901177b
Jason Deng <foralief@gmail.com>
2021-03-03 23:32:57 -0600

    Adding remote database connection

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	HOST: "127.0.0.1",
-	USER: "",
-	PASSWORD: "null",
-	DB: "hourglass-billing",
+	HOST: "isilo.db.elephantsql.com",
+	USER: "gjqxwhtp",
+	PASSWORD: "FIAgXtIIcvzs0DXsDZ40mGd0b0cgqS1e",
+	DB: "gjqxwhtp",
 	dialect: "postgres",
 	pool: {
 		max: 5,
